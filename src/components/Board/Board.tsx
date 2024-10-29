c644314b8ed44b6541debbb7c99b777e6d07f5a6
Nader Frigui <nadfri@gmail.com>
2024-10-29 14:52:01 +0100

    Fix:  replay animation Tiles

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -6,10 +6,12 @@ import { LINES } from '@/utils/init';
 export default function Board({ board }: { board: TileType[][] }) {
   const style = { gridTemplateColumns: `repeat(${LINES}, 1fr)` };
 
+  /*Unique key to replay each tile's animation*/
+
   return (
     <div className="Board fade-in" style={style}>
-      {board.flat().map((tile, position: number) => (
-        <Tile key={position} tile={tile} />
+      {board.flat().map((tile, index: number) => (
+        <Tile key={`${index}-${Date.now()}`} tile={tile} />
       ))}
     </div>
   );
