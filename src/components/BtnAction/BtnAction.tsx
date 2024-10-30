b73cb8ebf7eac74e7ebb1c9088bfdf38904e938a
Nader Frigui <nadfri@gmail.com>
2024-10-30 20:47:52 +0100

    Feat: Full screen button

--- src/components/BtnAction/BtnAction.tsx
+++ src/components/BtnAction/BtnAction.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import ReloadIcon from '../ReloadIcon/ReloadIcon';
 import './BtnAction.scss';
+import ScreenIcon from '../ScreenIcon/ScreenIcon';
+import ExitIcon from '../ExitIcon/ExitIcon';
 
 type Props = {
   reload: () => void;
@@ -8,8 +11,28 @@ type Props = {
 };
 
 export default function BtnAction({ reload, backPrevBoard, canBack }: Props) {
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const toggleFullscreen = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement
+        .requestFullscreen()
+        .then(() => setIsFullscreen(true))
+        .catch((err) => console.error(err.message));
+    } else {
+      document
+        .exitFullscreen()
+        .then(() => setIsFullscreen(false))
+        .catch((err) => console.error(err.message));
+    }
+  };
+
   return (
     <div className="BtnAction">
+      <button onClick={toggleFullscreen} className="btn-full">
+        {isFullscreen ? <ExitIcon /> : <ScreenIcon />}
+      </button>
+
       <button className="btn-reload" onClick={reload}>
         <ReloadIcon />
       </button>
