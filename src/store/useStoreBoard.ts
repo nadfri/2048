61d982917225e1bc096db8e9c76bc5e7fd3b5324
Nader Frigui <nadfri@gmail.com>
2024-11-09 20:37:34 +0100

    Refacto: Use Persist to handle localStorage

--- src/store/useStoreBoard.ts
+++ src/store/useStoreBoard.ts
@@ -1,6 +1,7 @@
-import { create } from 'zustand';
 import { newBoard } from '@/utils/init';
-import { getFromStorage, saveInStorage } from '@/utils/storage';
+import { create } from 'zustand';
+import { persist, PersistOptions } from 'zustand/middleware';
+import { StoreStateType } from '@/types/types';
 import {
   resetTileStates,
   transposeArray,
@@ -9,138 +10,117 @@ import {
   addNewNumberToBoard,
   directions,
 } from '@/utils/utils';
-import { StoreStateType } from '@/types/types';
 
-const storage = getFromStorage();
+const persistConfig: PersistOptions<StoreStateType> = {
+  name: '2048-NADFRIJS',
+};
 
-export const useStoreBoard = create<StoreStateType>((set, get) => ({
-  board: storage?.board || newBoard(),
-  prevBoard: storage?.prevBoard || null,
-  canBack: storage?.canBack || false,
-  score: storage?.score || 0,
-  prevScore: storage?.prevScore || 0,
-  highScore: storage?.highScore || 0,
-  maxValue: storage?.maxValue || 0,
+export const useStoreBoard = create<
+  StoreStateType,
+  [['zustand/persist', StoreStateType]]
+>(
+  persist(
+    (set, get) => ({
+      board: newBoard(),
+      prevBoard: null,
+      canBack: false,
+      score: 0,
+      prevScore: 0,
+      highScore: 0,
+      maxValue: 0,
 
-  /* Setters */
-  setBoard: (board) => set({ board }),
-  setPrevBoard: (prevBoard) => set({ prevBoard }),
-  setCanBack: (canBack) => set({ canBack }),
-  setScore: (score) => set({ score }),
-  setPrevScore: (prevScore) => set({ prevScore }),
-  setHighScore: (highScore) => set({ highScore }),
-  setMaxValue: (maxValue) => set({ maxValue }),
+      /* Setters */
+      setBoard: (board) => set({ board }),
+      setPrevBoard: (prevBoard) => set({ prevBoard }),
+      setCanBack: (canBack) => set({ canBack }),
+      setScore: (score) => set({ score }),
+      setPrevScore: (prevScore) => set({ prevScore }),
+      setHighScore: (highScore) => set({ highScore }),
+      setMaxValue: (maxValue) => set({ maxValue }),
 
-  /* Actions */
-  reload: () => {
-    set((state) => {
-      const newState = {
-        ...state,
-        board: newBoard(),
-        prevBoard: null,
-        canBack: false,
-        score: 0,
-        prevScore: 0,
-        maxValue: 0,
-      };
-
-      saveInStorage(newState);
-
-      return newState;
-    });
-  },
-
-  backPrevBoard: () => {
-    set((state) => {
-      if (state.prevBoard) {
-        const newState = {
+      /* Actions */
+      reload: () => {
+        set((state) => ({
           ...state,
+          board: newBoard(),
           prevBoard: null,
-          board: state.prevBoard,
           canBack: false,
-          score: state.prevScore,
+          score: 0,
           prevScore: 0,
-          maxValue: getMaxValue(state.prevBoard),
-        };
+          maxValue: 0,
+        }));
+      },
 
-        saveInStorage(newState);
+      backPrevBoard: () => {
+        set((state) => {
+          if (state.prevBoard) {
+            return {
+              ...state,
+              prevBoard: null,
+              board: state.prevBoard,
+              canBack: false,
+              score: state.prevScore,
+              prevScore: 0,
+              maxValue: getMaxValue(state.prevBoard),
+            };
+          }
+          return state;
+        });
+      },
 
-        return newState;
-      }
-      return state;
-    });
-  },
+      updateMove: (finalBoard) => {
+        set((state) => ({
+          ...state,
+          prevBoard: state.board,
+          board: finalBoard,
+          canBack: true,
+          maxValue: getMaxValue(finalBoard),
+        }));
+      },
 
-  updateMove: (finalBoard) => {
-    set((state) => {
-      const newState = {
-        ...state,
-        prevBoard: state.board,
-        board: finalBoard,
-        canBack: true,
-        score: state.score,
-        highScore: state.highScore,
-        maxValue: getMaxValue(finalBoard),
-      };
+      updateScore: (scoreGained: number) =>
+        set((state) => ({
+          score: state.score + scoreGained,
+          prevScore: state.score,
+          highScore: Math.max(state.score + scoreGained, state.highScore),
+        })),
 
-      saveInStorage(newState);
+      handleMove: (direction) => {
+        const { board, updateMove } = get();
 
-      return newState;
-    });
-  },
+        if (!directions.includes(direction)) return;
 
-  updateScore: (scoreGained: number) =>
-    set((state) => {
-      const newPrevScore = state.score;
-      const newScore = state.score + scoreGained;
+        const isVerticalMove =
+          direction === 'ArrowDown' || direction === 'ArrowUp';
 
-      const newHighScore =
-        newScore > state.highScore ? newScore : state.highScore;
+        /***#1 Reset the tile states***/
+        const resetedBoard = resetTileStates(board);
 
-      return {
-        score: newScore,
-        prevScore: newPrevScore,
-        highScore: newHighScore,
-      };
+        /***#2 Change Orientation for Vertical move***/
+        const orientedBoard = isVerticalMove
+          ? transposeArray(resetedBoard)
+          : resetedBoard;
+
+        /***#3 Update Board and Score after slide move***/
+        const updatedBoard = orientedBoard.map((line) =>
+          slideLine(line, direction),
+        );
+
+        const isBoardChanged =
+          JSON.stringify(orientedBoard) !== JSON.stringify(updatedBoard);
+
+        if (!isBoardChanged) return;
+
+        /***#4 Add a new number***/
+        const boardWithNewNumber = addNewNumberToBoard(updatedBoard);
+        const finalBoard = isVerticalMove
+          ? transposeArray(boardWithNewNumber)
+          : boardWithNewNumber;
+
+        /***#5 Update State***/
+        updateMove(finalBoard);
+      },
     }),
-
-  handleMove: (direction) => {
-    const { board, updateMove } = get();
-
-    if (!directions.includes(direction)) return;
-
-    const isVerticalMove = direction === 'ArrowDown' || direction === 'ArrowUp';
-
-    /***#1 Reset the tile states***/
-    const resetedBoard = resetTileStates(board);
-
-    /***#2 Change Orientation for Vertical move***/
-    const orientedBoard = isVerticalMove
-      ? transposeArray(resetedBoard)
-      : resetedBoard;
-
-    /***#3 Update Board and Score after slide move***/
-    const updatedBoard = orientedBoard.map((line) =>
-      slideLine(line, direction),
-    );
-
-    const isBoardChanged = orientedBoard.some((row, rowIndex) =>
-      row.some(
-        (tile, colIndex) =>
-          tile.value !== updatedBoard[rowIndex][colIndex].value,
-      ),
-    );
-
-    if (!isBoardChanged) return;
-
-    /***#4 Add a new number***/
-    const boardWithNewNumber = addNewNumberToBoard(updatedBoard);
-
-    const finalBoard = isVerticalMove
-      ? transposeArray(boardWithNewNumber)
-      : boardWithNewNumber;
-
-    /***#5 Update State***/
-    updateMove(finalBoard);
-  },
-}));
+    persistConfig,
+  ),
+);
